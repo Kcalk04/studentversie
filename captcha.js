11a70f897fe97e5275135deb7594d03eca79feb8
Kiran <kiran.veenendaal.phone@gmail.com>
2024-02-26 23:38:15 +0100

    WIP validation

--- captcha.js
+++ captcha.js
@@ -12,8 +12,6 @@ function onSubmit(e) {
         grecaptcha.execute("6LfHg4ApAAAAAGPJBCsxEPiZaIpSx3Ziewqv-13j", { action: 'submit' }).then(async function (token) {
             try {
                 // Verstuur het eerst naar jouw eigen server.
-                // Voor dit voorbeeld is een nodejs server bijgevoegd (Zie map server).
-                // Je kunt dit voor je showcase ook aanpassen door je eigen server project (bijv. ASP.NET) te gebruiken.
                 const response = await fetch('http://localhost:3001/send-email', {
                     method: "POST",
                     body: JSON.stringify({
@@ -22,43 +20,14 @@ function onSubmit(e) {
                         achternaam: document.getElementById('achternaam').value,
                         email: document.getElementById('email').value,
                         phoneNumber: document.getElementById('phoneNumber').value,
-                        messageBody: document.getElementById('messageBody').value
+                        messageBody: document.getElementById('messageBody').value,
+                        subject: document.getElementById('subject').value
                     }),
                     headers: {
                         'Accept': 'application/json',
                         'Content-Type': 'application/json'
                     }
                 });
-
-                // handel het resultaat af en bepaal of je te maken hebt met een mens of een bot.
-                // lees hier meer over de response vanuit Google: https://developers.google.com/recaptcha/docs/v3#site_verify_response
-                const result = await response.json();
-                let humanFactor;
-                let isHuman;
-                // je bepaalt zelf wat je doet met de score, 0.5 is slechts een voorbeeldwaarde
-                if (result.score > 0.5) {
-                    humanFactor = 'Het lijkt erop dat je een mens bent, je score is: ' + result.score;
-                    isHuman = true;
-                }
-                else {
-                    humanFactor = 'Het lijkt erop dat je geen mens bent, je score is: ' + result.score;
-                    isHuman = false;
-                }
-
-                // Onderstaande code: pas aan naar je eigen smaak
-                const sectionPersonalia = document.querySelector('.personalia');
-                let pResult = document.createElement('p');
-                pResult.innerHTML = humanFactor;
-                sectionPersonalia.appendChild(pResult);
-
-                if (isHuman) {
-                    // Wacht 3 seconden en verstuur dan het formulier alsnog
-                    setTimeout(() => {
-                        // form.submit();
-                        pResult.innerHTML = 'Het formulier is verzonden!';
-                    }, 3000
-                    );
-                }
             }
             catch (e) {
                 console.log('Het verzenden van de captcha is mislukt: ' + e.message)
--- server.js
+++ server.js
@@ -18,41 +18,27 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.post('/send-email', async (req, res) => {
-// const {voornaam, achternaam, email, phoneNumber, messageBody} = req.body;
-
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail', 
-//     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS,
-//     },
-// });
-
-// const mailOptions = {
-//     from: `"${voornaam} ${achternaam}" ${email}`,
-//     to: "kiran.veenendaal.phone@gmail.com",
-//     subject: "Your Subject Here", 
-//     text: `${messageBody} send response to: ${email}`,
-//     html: `<p>${messageBody}</p> <br> <p>send response to: ${email}</p>`
-// }
-
-// try {
-//     await transporter.sendMail(mailOptions);
-//     console.log("email sent sucsessfully");
-//     res.send('Email sent successfully');
-// } catch(error){
-//     console.error(error);
-// }
-
-// });
 
 app.post('/send-email', async (req, res) => {
     let token = req.body.token;
     // Vul hier je secret key in van Google reCAPTCHA, check dat je dit op een veilige (security) manier doet.
     let secret = process.env.KEY_SECRET_RECAPTCHA;
     // Verstuur de gegevens naar de Google Api
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     try {
+        const { voornaam, achternaam, email, phoneNumber, subject, messageBody } = req.body;
+        debugger;
+        if(subject && subject.length > 200) {
+            res.send("Onderwerp is langer dan 200 karakters");
+            return;
+        } else if (messageBody && messageBody.length > 600) {
+            res.send("Bericht is langer dan 600 karakters");
+            return;
+        } else if (email && !(emailRegex.test(email))){
+            res.send("Email is niet geldig");
+            return;
+        }
         const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`, {
             method: "POST",
             body: JSON.stringify({
@@ -73,7 +59,7 @@ app.post('/send-email', async (req, res) => {
             res.send('Captcha failed');
             return 0;
         }
-        const { voornaam, achternaam, email, phoneNumber, messageBody } = req.body;
+        
 
         const transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -86,9 +72,9 @@ app.post('/send-email', async (req, res) => {
         const mailOptions = {
             from: `"${voornaam} ${achternaam}" ${email}`,
             to: "kiran.veenendaal.phone@gmail.com",
-            subject: "Your Subject Here",
+            subject: `${subject}`,
             text: `${messageBody} send response to: ${email}`,
-            html: `<p>${messageBody}</p> <br> <p>send response to: ${email}</p>`
+            html: `<p>${messageBody}</p> <br> <p>send response to: ${email} or call: ${phoneNumber}</p>`
         }
 
         try {
@@ -97,6 +83,7 @@ app.post('/send-email', async (req, res) => {
             res.send('Email sent successfully');
         } catch (error) {
             console.error(error);
+            res.send("Mail wasn't sent successfully due to error!")
         }
 
     }
