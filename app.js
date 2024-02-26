65be44ae5ddf1b30591cb94cb683b9aba3bb6e18
Kiran Kok <s1190487@student.windesheim.nl>
2024-02-26 20:04:58 +0100

    WIP Captcha

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-const nodeMailer = require('nodemailer');
-require('dotenv').config()
-console.log(process.env)
+// const nodeMailer = require('nodemailer');
+// require('dotenv').config()
+// console.log(process.env)
 
-const transporter = nodeMailer.createTransport(
-    {
-        service: 'gmail',
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    }
-);
+// const transporter = nodeMailer.createTransport(
+//     {
+//         service: 'gmail',
+//         host: "smtp.gmail.com",
+//         port: 587,
+//         secure: false,
+//         auth: {
+//             user: process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASS,
+//         },
+//     }
+// );
 
-const mailOptions = {
-    from: {
-        name: 'Kiran Kok',
-        address: "showcase.response@gmail.com"
-    }, 
-    to: "kiran.veenendaal.phone@gmail.com",
-    subject: "onderwerp",
-    text: "Hello world?!",
-    html: "<p>Hello world?</p>"
-}
+// const mailOptions = {
+//     from: {
+//         name: 'Kiran Kok',
+//         address: "showcase.response@gmail.com"
+//     }, 
+//     to: "kiran.veenendaal.phone@gmail.com",
+//     subject: "onderwerp",
+//     text: "Hello world?!",
+//     html: "<p>Hello world?</p>"
+// }
 
-const sendMail = async (transporter, mailOptions) => {
-    try{
-        await transporter.sendMail(mailOptions);
-        console.log('email has been sent')
-    } catch(error) {
-        console.error(error);
-    }
-}
+// const sendMail = async (transporter, mailOptions) => {
+//     try{
+//         await transporter.sendMail(mailOptions);
+//         console.log('email has been sent')
+//     } catch(error) {
+//         console.error(error);
+//     }
+// }
 
-sendMail(transporter, mailOptions);
+// sendMail(transporter, mailOptions);
--- server.js
+++ server.js
@@ -4,41 +4,93 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3001;
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// app.post('/send-email', async (req, res) => {
+// const {voornaam, achternaam, email, phoneNumber, messageBody} = req.body;
+
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail', 
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
+// const mailOptions = {
+//     from: `"${voornaam} ${achternaam}" ${email}`,
+//     to: "kiran.veenendaal.phone@gmail.com",
+//     subject: "Your Subject Here", 
+//     text: `${messageBody} send response to: ${email}`,
+//     html: `<p>${messageBody}</p> <br> <p>send response to: ${email}</p>`
+// }
+
+// try {
+//     await transporter.sendMail(mailOptions);
+//     console.log("email sent sucsessfully");
+//     res.send('Email sent successfully');
+// } catch(error){
+//     console.error(error);
+// }
+
+// });
+    let result;
 app.post('/send-email', async (req, res) => {
-const {voornaam, achternaam, email, phoneNumber, messageBody} = req.body;
+    token = grecaptcha.getResponse();
+    // Vul hier je secret key in van Google reCAPTCHA, check dat je dit op een veilige (security) manier doet.
+    let secret ="6LcbzIApAAAAAKAVxqPadqA-db8-edCm2zJ5jilY";
+    // Verstuur de gegevens naar de Google Api
+    try {
+        const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`, {
+            method: "POST",
+            body: JSON.stringify({
+                secret: secret,
+                response: token
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        // De response vanuit Google (meer info: https://developers.google.com/recaptcha/docs/v3#site_verify_response):
+        result = await response.json();
+       // Stuur het resultaat weer terug naar je client
+    }
+    catch (e) {
+        console.log(e);
+    }
+    if(result.pass){
+        const {voornaam, achternaam, email, phoneNumber, messageBody} = req.body;
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail', 
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
-
-const mailOptions = {
-    from: `"${voornaam} ${achternaam}" <${email}>`,
-    to: "kiran.veenendaal.phone@gmail.com",
-    subject: "Your Subject Here", 
-    text: `${messageBody}`,
-    html: `<p>${messageBody}</p>`
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+    
+    const mailOptions = {
+        from: `"${voornaam} ${achternaam}" ${email}`,
+        to: "kiran.veenendaal.phone@gmail.com",
+        subject: "Your Subject Here", 
+        text: `${messageBody} send response to: ${email}`,
+        html: `<p>${messageBody}</p> <br> <p>send response to: ${email}</p>`
+    }
+    
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("email sent sucsessfully");
+        res.send('Email sent successfully');
+    } catch(error){
+        console.error(error);
+    }
 }
-
-try {
-    await transporter.sendMail(mailOptions);
-    console.log("email sent sucsessfully");
-    res.send('Email sent successfully');
-} catch(error){
-    console.error(error);
-}
-
 });
 
 app.listen(port, () => {
-    console.log('Server running on port ${port}')
+    console.log(`Server running on port ${port}`)
 })
